f0a250eaa303a3b9f6b0f68613c8d88460445b15
AkanoCA <akano.ca@yahoo.com>
2020-01-27 17:33:56 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 window.addEventListener('DOMContentLoaded', function() {
 
-    let tab = document.querySelectorAll('.info-header-tab');
-    let info = document.querySelector('.info-header');
+    let tab = document.querySelectorAll('.info-header-tab'); 
+    let info = document.querySelector('.info-header');                  
     let tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent (a) {
@@ -35,4 +35,56 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //------Timer
+
+    let deadLine = '2020-01-28';
+
+    function getTimeRemaining (endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date());
+
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor(t / (1000 * 60 * 60));
+
+        return {
+            'total'     : t,
+            'hours'     : hours,
+            'minutes'   : minutes,
+            'seconds'   : seconds
+        };
+    }
+
+    function setClock (id, endTime) {
+        let timer = document.querySelector('#' + id);
+        let hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+            function updateClock () {
+                let t = getTimeRemaining(endTime);
+                
+                hours.textContent = setTime(t.hours);
+                minutes.textContent = setTime(t.minutes);
+                seconds.textContent = setTime(t.seconds)    ;
+
+                if(t.total <= 0) {
+                    hours.textContent = '00';
+                    minutes.textContent = '00';
+                    seconds.textContent = '00';
+                    clearInterval(timeInterval);
+                }
+
+                function setTime (time) {
+                    if (time > 9){
+                        return time;
+                    } else {
+                        return ('0' + time);
+                    }
+                }
+            }
+    }
+
+    setClock('timer', deadLine);
 });
