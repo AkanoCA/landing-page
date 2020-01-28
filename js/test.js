2003eedbe9befb7291862f7725e77452f0fc1968
AkanoCA <akanoca@yahoo.com>
2020-01-28 14:48:38 +0300

    ES6 refactoring

--- js/test.js
+++ js/test.js
@@ -1,8 +1,31 @@
 'use strict';
 
-let age = document.getElementById('age');
+document.write('<div style="font-size: 30px"> Hello world!</div>');
 
-function showUser(surname, name) {
-	alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+class Options {
+	constructor(height, width, bg, fontSize, textAlign, text) {
+		this.height = height;
+		this.width = width;
+		this.bg = bg;
+		this.fontSize = fontSize;
+		this.textAlign = textAlign;
+		this.text = text;
+	}
+	createDiv () {
+		let newDiv = document.createElement('div');
+		document.body.appendChild(newDiv);
+
+		let param = `height:${this.height}px;
+					 width:${this.width}px;
+					 background:${this.bg};
+					 font-size:${this.fontSize}px;
+					 text-align:${this.textAlign}`;
+		newDiv.style.cssText = param;
+		newDiv.textContent = this.text;
+	}
 }
-showUser('Smith', 'John');
+
+
+let block = new Options(120, 300, '#eaeafa', 36, 'center', 'I Like It');
+
+block.createDiv();
